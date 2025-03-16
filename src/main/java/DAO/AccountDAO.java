36590137f3e78356e8ee71ab1061adb70a570353
Sebastien Adam <sebastien1adam@gmail.com>
2025-03-16 02:27:05 +0000

    making imports more specific

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,7 +1,13 @@
 package DAO;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import Util.ConnectionUtil;
+
 import Model.Account;
 
 public class AccountDAO {
