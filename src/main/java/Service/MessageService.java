d354445276f90d9ede48114803db65e292a00f5b
Sebastien Adam <sebastien1adam@gmail.com>
2025-03-16 02:29:06 +0000

    implement createMessage

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,26 @@
 package Service;
 
+import DAO.MessageDAO;
+import DAO.AccountDAO;
+import Model.Message;
+
 public class MessageService {
 
+    private MessageDAO messageDAO;
+    private AccountDAO accountDAO;
+
+    public MessageService(){
+        this.messageDAO = new MessageDAO(); 
+        this.accountDAO = new AccountDAO();
+    }
+    
+    public Message createMessage(Message message){
+        return meetsMessageRequirements(message) ? messageDAO.insertMessage(message) : null;
+    }
+
+    public boolean meetsMessageRequirements(Message message){
+        return !message.getMessage_text().isBlank() &&
+                message.getMessage_text().length() < 255 &&
+                accountDAO.getAccountById(message.getPosted_by()) != null;
+    }
 }
