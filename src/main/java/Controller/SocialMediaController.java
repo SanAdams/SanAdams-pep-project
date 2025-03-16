SanAdams/SanAdams-pep-project

commit 5716e5c77d5f3ead4c4261ade9be5245e90593b5

fixed getAllMessages

src/main/java/DAO/MessageDAO.java
@@ -82,7 +82,7 @@ public class MessageDAO {
         List<Message> messages = new ArrayList<Message>();
         
         try {
-            String sql = "select * from messages;";
+            String sql = "select * from message;";
             Statement pstmt = connection.createStatement();
             
             ResultSet rs = pstmt.executeQuery(sql);

commit 5b915c4093bcd78e193074e504f7808276cd9ef5

fixed issue introduced in commit 5716e5c

src/main/java/Controller/SocialMediaController.java
@@ -4,6 +4,7 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -142,8 +143,24 @@ public class SocialMediaController {
      * 
      * @param ctx
      */
-    private void updateMessageGivenIdHandler(Context ctx){
+    @SuppressWarnings("unchecked")
+    private void updateMessageGivenIdHandler(Context ctx) throws JsonProcessingException{
+        String messageIdStr = ctx.pathParam("message_id");
+        int messageId = Integer.parseInt(messageIdStr);
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        
+        //Convert the request into a map and extract the message_text field
+        Map<String, String> requestMap = objectMapper.readValue(ctx.body(), Map.class);
+        String updatedMessageString = requestMap.get("message_text");
+        
+        Message updatedMessage = messageService.updateMessageGivenId(messageId, updatedMessageString);
+        if (updatedMessage != null){
+            ctx.json(objectMapper.writeValueAsString(updatedMessage));
+        }
+        else{
+            ctx.status(400);
+        }
     }
 
     /**

src/main/java/DAO/MessageDAO.java
@@ -99,4 +99,18 @@ public class MessageDAO {
         
         return messages;
     }
+
+    public void updateMessageGivenId(int id, String updatedMessageString){
+        try {
+            String sql = "update message " +
+                         "set message_text = ? " +
+                         "where message_id = ?;";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, updatedMessageString);
+            pstmt.setInt(2, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }

src/main/java/Service/MessageService.java
@@ -42,4 +42,17 @@ public class MessageService {
         }
         return messageAsStoredInDB;
     }
+
+    public Message updateMessageGivenId(int id, String updatedMessageString){
+        if (messageDAO.getMessageGivenId(id) != null && meetsUpdateRequirements(updatedMessageString)){
+            messageDAO.updateMessageGivenId(id, updatedMessageString);
+            return messageDAO.getMessageGivenId(id);
+        }
+        return null;
+    }
+
+    public boolean meetsUpdateRequirements(String updatedMessageString){
+        return !updatedMessageString.isBlank() &&
+                updatedMessageString.length() <= MAX_MESSAGE_LENGTH;
+    }
 }