c02281d95c51ac87fa7365f2cf29fab931e282a4
Sebastien Adam <sebastien1adam@gmail.com>
2025-03-16 06:36:55 +0000

    implemented getAllMessages

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -6,6 +6,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.h2.command.Prepared;
 import org.h2.command.ddl.PrepareProcedure;
 
@@ -73,5 +76,27 @@ public class MessageDAO {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public List<Message> getAllMessages(){
+        List<Message> messages = new ArrayList<Message>();
+        
+        try {
+            String sql = "select * from messages;";
+            Statement pstmt = connection.createStatement();
+            
+            ResultSet rs = pstmt.executeQuery(sql);
+            while(rs.next()){
+                Message message = new Message((int) rs.getLong("message_id"),
+                                            rs.getInt("posted_by"),
+                                            rs.getString("message_text"),
+                                            rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return messages;
     }
 }
